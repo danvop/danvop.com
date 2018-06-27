403cfb77f769df64f14f76b27011e5b1c3d0f00a
Попов Данила Александрович <danvop@gmail.com>
2018-06-27 17:40:11 +1000

    second commit

---
+++ content.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-dark bg-info">
+  <a class="navbar-brand" href="#">Danvop development team</a>
+</nav>
+
+<div class="row">
+    
+
+
+    <div class='p-5 col-md mb-3'>
+    <h3>We make solid minimalist design websites and custom Laravel applications</h3>  
+    </div>
+
+    <div class="col-md">
+        <div class="media col-sm mb-3">
+          <img class="align-self-center mr-2  rounded" src="img/danvop_founder.jpg" alt="Danila Popov photo">
+            <div class="media-body my-auto">
+                <h5 class="mt-0">Danila Popov</h5>
+                <p class="text-black-50">Founder and CEO</p>
+                <p class="text-black-50"><i>Moscow, Russia</i></p>
+            </div>
+        </div>
+        <div class="media col-sm mb-3">
+          <img class="align-self-center mr-2  rounded" src="img/omar kumar_v2.jpg" alt="Omar Kumar photo">
+          <div class="media-body my-auto">
+            <h5 class="mt-0 strong">Omar Kumar</h5>
+            <p class="text-black-50">BackEnd developer</p>
+            <p class="text-black-50"><i>Delhi, India</i></p>
+          </div>
+        </div>
+        
+        <div class="media col-sm">
+          <img class="align-self-center mr-2  rounded" src="img/rulon aboev_v2.jpg" alt="Rulon Aboev photo">
+          <div class="media-body my-auto">
+            <h5 class="mt-0">Rulon Aboev</h5>
+            <p class="text-black-50">Art and Design</p>
+            <p class="text-black-50"><i>Antwerp, Belgium</i></p>
+
+          </div>
+        </div>
+
+    </div>    
+
+</div>
+
+
+
+<footer class="footer bg-info text-white text-center mt-3">
+<a class="text-white align-middle" href="#"><i class="fab fa-twitter" style="font-size:30px"></i></a>
+<a class="text-white align-middle" href="#"><i class="fab fa-github" style="font-size:30px"></i></a>
+</footer>
+
