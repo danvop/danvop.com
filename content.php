2a1478b348b0e2f5aa6a3ff8bc1b6012c074fa22
Попов Данила Александрович <danvop@gmail.com>
2018-06-27 18:01:18 +1000

    carousel

--- content.php
+++ content.php
@@ -5,11 +5,42 @@
 <div class="row">
     
 
-
-    <div class='p-5 col-md mb-3'>
+<div class="col-md">
+    <!-- marketing slogan -->
+    <div class='p-5 mb-3'>
     <h3>We make solid minimalist design websites and custom Laravel applications</h3>  
     </div>
 
+    <!-- Carousel -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+</div>
+
+    <!-- faces -->
     <div class="col-md">
         <div class="media col-sm mb-3">
           <img class="align-self-center mr-2  rounded" src="img/danvop_founder.jpg" alt="Danila Popov photo">
