0a1aeaa683bf379e9adbe3cba21493f0d06a1c25
danvop <danvop@gmail.com>
2018-08-12 19:41:55 +1000

    init comment

---
+++ content.php
@@ -0,0 +1,60 @@
+<div class="container">
+  <header>
+    <h3>Brand</h3>
+  </header>
+  <div class="content-container">
+  <main>
+    <div class="about-brand">
+      <h2>We make minimalist website designs and custom online applications</h2>
+    </div> 
+    <!-- carousel section -->
+    <h3>Our projects</h3>
+    <hr>
+    <div id="danvopCarousel"></div>
+        
+    
+  </main>
+  <aside>
+      <h3>Our team</h3>
+      <hr>
+      <div class="media-container">
+      <div class="media">
+        <div class="media-img">
+          <img src="img/danvop_founder_100.png" alt="">
+        </div>
+        <div class="media-content">
+          <h3>Danila Popov</h3>
+          <p>Founder and CEO</p>
+          <p><i>Moscow, Russia</i></p>
+        </div>
+      </div>
+      <div class="media">
+        <div class="media-img">
+          <img src="img/omar kumar_v2.jpg" alt="">
+        </div>
+        <div class="media-content">
+          <h3>Omar Kumar</h3>
+          <p>BackEnd developer</p>
+          <p><i>Delhi, India</i></p>
+        </div>
+      </div>
+      <div class="media">
+        <div class="media-img">
+          <img src="img/rulon aboev_v2.jpg" alt="">
+        </div>
+        <div class="media-content">
+          <h3>Rulon Aboev</h3>
+          <p>Art and Design</p>
+          <p><i>Antwerp, Belgium</i></p>
+        </div>
+      </div>
+      </div>
+    </aside>
+    </div>
+  <footer>
+    
+    <a class="" href="https://twitter.com/danilaapopov" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a class="" href="https://github.com/danvop" target="_blank"><i class="fab fa-github"></i></a>
+    
+  </footer>
+</div>
